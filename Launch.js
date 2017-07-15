627a36c7a1f3d43eba93d28906d3411d5c68ef0c
Kyle MacKinnon <kk.mackinnon@gmail.com>
2017-07-15 20:57:23 +1200

    Weather API connected. Responds with Auckland's weather.

--- Launch.js
+++ Launch.js
@@ -1,8 +1,9 @@
 
 var restify = require('restify');
 var builder = require('botbuilder');
+var http = require('http');
 
-// Setup Restify Server
+// Setup and run Restify web server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
@@ -14,10 +15,40 @@ var connector = new builder.ChatConnector({
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
-// Listen for messages from users 
+// Listen for messages from users at URL
 server.post('/api/messages', connector.listen());
 
-// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
-});
+// Create a function for handling messages
+function messageHandler(session) {
+
+    // Location to find weather for
+    var city = 'Auckland';
+
+    // HTTP request options
+    var options = {
+        host: 'api.openweathermap.org',
+        port: 80,
+        path: '/data/2.5/weather?APPID=7eced897468f2ccff50a9c4decc7c529&q=' + city,
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
+    };
+
+    // HTTP request callback
+    function callback(result) {
+
+        // Retrieve the JSON response
+        result.on('data', function(data) {
+            var response = JSON.parse(data);
+            var city = response.name;
+            var weather  = response.weather[0];
+
+            session.send('## ' + city + '![](http://openweathermap.org/img/w/' + response.weather[0].icon +'.png)' + "\n" + weather.main);
+        });
+    }
+
+    // Make request
+    http.request(options, callback).end();
+}
+
+// Run bot using message handler
+var bot = new builder.UniversalBot(connector, messageHandler)
